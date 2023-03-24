ae1a44f8d0bbfffd727d86b8405df69adc17290f
mahfuz khan <mahfuzpust@gmail.com>
2023-03-24 23:07:27 +0600

    Add project files.

---
+++ RegisterLoginASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegisterLoginASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
